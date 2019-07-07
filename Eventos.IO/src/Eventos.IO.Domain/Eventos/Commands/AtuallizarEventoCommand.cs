bc44882c5eabfe4e032faef52c1dc0e23c7a7ff8
Marcelo Oliveira Calumbi <marcelo.calumby@gmail.com>
2019-07-07 18:16:32 -0300

    [master] entrega do projeto Eventos.IO

---
+++ Eventos.IO/src/Eventos.IO.Domain/Eventos/Commands/AtuallizarEventoCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.Eventos.Commands
+{
+    public class AtuallizarEventoCommand : BaseEventoCommand
+    {
+        public AtuallizarEventoCommand(Guid id, string nome, string descricaoCurta, 
+            string descricaoLonga, DateTime dataInicio, DateTime dataFim, bool gratuito, 
+            decimal valor, bool online, string nomeEmpresa)
+        {
+            Id = id;
+            Nome = nome;
+            DescricaoCurta = descricaoCurta;
+            DescricaoLonga = descricaoLonga;
+            DataInicio = dataInicio;
+            DataFim = dataFim;
+            Gratuito = gratuito;
+            Valor = valor;
+            Online = online;
+            NomeDaEmpresa = nomeEmpresa;
+        }
+    }
+}
